c33a69ab557af730e29ce6ddc4994a35564486ad
frogstat <frogstatftw@gmail.com>
2025-10-21 12:18:33 +0200

    Change args to argsor

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] argsor) {
         System.out.println("Hello Vanja, let's rock and/or roll!");
     }
 }
